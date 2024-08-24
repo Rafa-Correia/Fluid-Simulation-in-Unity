6bee47ff0ff821eb3e65f6750e53d1d68e6eb120
Rafa-Correia <rafaelazevedocorreia04@gmail.com>
2024-08-24 14:16:24 +0100

    Added tools used to draw things (in this case points)

--- FSim/Assets/Scripts/FluidBehaviour.cs
+++ FSim/Assets/Scripts/FluidBehaviour.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VidTools;
+using VidTools.Vis;
 
 public class FluidBehaviour : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Draw.Point(Vector3.zero, 0.0f, Color.white);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Draw.Point(Vector3.zero, 1.0f, Color.white);
     }
 }
